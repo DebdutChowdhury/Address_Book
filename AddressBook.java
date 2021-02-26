e4cb3c83328d89ee8134c209a3e781991c4b80d0
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-02-26 16:47:26 +0530

    Refactor: Here no duplicate entry of the same person in the particular addressbook

--- AddressBook.java
+++ AddressBook.java
@@ -9,23 +9,28 @@ public class AddressBook {
 
         System.out.println("Enter the first name: ");
         String fstname = sc.next();
-        System.out.println("Enter the last name: ");
-        String lstname = sc.next();
-        System.out.println("Enter the address: ");
-        String address = sc.next();
-        System.out.println("Enter the city: ");
-        String city = sc.next();
-        System.out.println("Enter the state: ");
-        String state = sc.next();
-        System.out.println("Enter the zip: ");
-        int zip = sc.nextInt();
-        System.out.println("Enter the phone number: ");
-        long ph_num = sc.nextLong();
-        System.out.println("Enter the email id: ");
-        String eml = sc.next();
+        if (checkDublcate(fstname)){
+            System.out.println("Its already exist");
+        }
+        else {
+            System.out.println("Enter the last name: ");
+            String lstname = sc.next();
+            System.out.println("Enter the address: ");
+            String address = sc.next();
+            System.out.println("Enter the city: ");
+            String city = sc.next();
+            System.out.println("Enter the state: ");
+            String state = sc.next();
+            System.out.println("Enter the zip: ");
+            int zip = sc.nextInt();
+            System.out.println("Enter the phone number: ");
+            long ph_num = sc.nextLong();
+            System.out.println("Enter the email id: ");
+            String eml = sc.next();
 
-        Contact contact = new Contact(fstname, lstname, address, city, state, zip, ph_num, eml);
-        contactList.add(contact);
+            Contact contact = new Contact(fstname, lstname, address, city, state, zip, ph_num, eml);
+            contactList.add(contact);
+        }
     }
 
     boolean editContactdetalis(String name){
@@ -60,6 +65,17 @@ public class AddressBook {
         for (Contact contact : contactList){
             if (contact.fstname.equals(name)){
                 contactList.remove(contact);
+                flag = 1;
+                break;
+            }
+        }
+        return flag == 1;
+    }
+
+    boolean checkDublcate(String fstname){
+        int flag = 0;
+        for (Contact list : contactList){
+            if (list.fstname.equals(fstname)){
                 flag = 1;
                 break;
             }
